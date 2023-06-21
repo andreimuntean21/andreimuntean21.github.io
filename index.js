fca72d29294bcfa851ce36a6873faa7c0fb83c5b
Andrei Muntean <andrei.muntean@fidasolutions.com>
2023-06-21 15:09:22 +0300

    dynamic click on menu (addEventListener)

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 var activePage = "home";
 
 function hide(id) {
-  document.getElementById(id).style.display = "none";
+  $("#" + id).style.display = "none";
 }
 function show(id) {
-  var page = document.getElementById(id);
+  var page = $(`#${id}`);
   page.style.display = "block";
+}
+
+function $(selector) {
+  return document.querySelector(selector);
 }
 
 function displayPage(id) {
   hide(activePage);
-  document
-    .querySelector(`#top-menu-bar a[data-page="${activePage}"]`)
-    .classList.remove("active");
+  $(`#top-menu-bar a[data-page="${activePage}"]`).classList.remove("active");
   activePage = id;
   show(id);
-  document
-    .querySelector(`#top-menu-bar a[data-page="${id}"]`)
-    .classList.add("active");
+  $(`#top-menu-bar a[data-page="${id}"]`).classList.add("active");
 }
 
 displayPage(activePage);
+
+function clickOnMenu(e) {
+  if (e.target.matches("a")) {
+    var id = e.target.dataset.page;
+    if (id) {
+      displayPage(id);
+    } else {
+      console.warn('please use <a data-page="pageid"> ');
+    }
+  }
+}
+
+$("#top-menu-bar").addEventListener("click", clickOnMenu);
